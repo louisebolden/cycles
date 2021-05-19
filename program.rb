46db039424c9fc08a3a2c45ef639c3518b412611
Louise Swift <louise@louiseswift.com>
2021-05-19 18:36:07 +0100

    Initial program

--- program.rb
+++ program.rb
@@ -3,8 +3,12 @@ class DisjointCycles
 
   def initialize(cycles, name)
     @cycles = cycles
-    @max = cycles.flatten.max
+    @max = cycles_flat.max
     @name = name
+  end
+
+  def cycles_flat
+    @cycles_flat ||= cycles.flatten
   end
 
   def next(input)
@@ -21,8 +25,35 @@ class DisjointCycles
   end
 
   def of(other_disjoint_cycles)
+    current_pointer = 1
+    stop_at = current_pointer
 
-    "#{name} ○ #{other_disjoint_cycles.name} = "
+    to_include = (2..max).to_a
+
+    new_disjoint_cycles = []
+    current_cycle = []
+
+    loop do
+      current_cycle.push(current_pointer)
+
+      next_pointer = self.next(other_disjoint_cycles.next(current_pointer))
+
+      to_include.delete(next_pointer)
+      current_pointer = next_pointer
+
+      if current_pointer == stop_at
+        new_disjoint_cycles.push(current_cycle)
+
+        current_pointer = to_include.shift
+        stop_at = current_pointer
+
+        break if to_include.length == 0
+
+        current_cycle = []
+      end
+    end
+
+    DisjointCycles.new(new_disjoint_cycles, "#{name} ○ #{other_disjoint_cycles.name}")
   end
 
   def print
@@ -37,21 +68,41 @@ class DisjointCycles
 end
 
 delta = DisjointCycles.new([
-  [1, 3, 5], [2, 4], [6]
+  [1, 3, 5, 7], [2, 4, 8, 12], [6, 9, 10, 11]
 ], 'δ')
 
 gamma = DisjointCycles.new([
-  [1, 6, 4, 5], [2], [3]
+  [1, 2, 4, 8], [3, 6, 9, 12], [5, 10], [7], [11]
 ], 'γ')
 
 puts delta.print
-# puts gamma.print
-# puts delta.of(gamma)
-puts 'delta.next(1): ' + delta.next(1).inspect
-puts 'delta.next(3): ' + delta.next(3).inspect
-puts 'delta.next(5): ' + delta.next(5).inspect
-puts 'delta.next(2): ' + delta.next(2).inspect
-puts 'delta.next(4): ' + delta.next(4).inspect
-puts 'delta.next(6): ' + delta.next(6).inspect
+puts gamma.print
+puts delta.of(gamma).print
+puts gamma.of(delta).print
 
-# ADD TESTS
+# TESTS
+def run_test(description, expected, received)
+  unless received == expected
+    raise "\nTEST FAILED: #{description}.\nExpected: #{expected}\nReceived: #{received}"
+  end
+end
+
+[
+  [3, delta.next(1)],
+  [5, delta.next(3)],
+  [7, delta.next(5)],
+  [4, delta.next(2)],
+  [8, delta.next(4)],
+  [9, delta.next(6)],
+  [6, delta.next(11)]
+].map do |expected_and_received|
+  expected, received = expected_and_received
+  run_test('delta.next should return the correct next element', expected, received)
+end
+
+[
+  [7, gamma.next(7)]
+].map do |expected_and_received|
+  expected, received = expected_and_received
+  run_test('gamma.next should return the correct next element', expected, received)
+end
